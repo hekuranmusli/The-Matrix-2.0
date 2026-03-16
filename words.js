958abe3f03ffd30366c0ae1245761adb8bd9599a
Hekuran Musli <hekuranmusli@icloud.com>
2026-03-16 19:57:11 +0100

    Extract vocabulary and characters to external words.js config file

---
+++ words.js
@@ -0,0 +1,19 @@
+// This file acts as a configuration dictionary.
+// You can add or remove special words here, and modify the base characters.
+
+const matrixConfig = {
+    // These are the special words that will randomly drop in the rain as easter eggs.
+    // Add your own words to this list!
+    "specialWords": [
+        "ARCOVERSE",
+        "Hekuran",
+        "Musli",
+        "NEO",
+        "TRINITY",
+        "MORPHEUS"
+    ],
+
+    // This is the pool of characters the algorithm picks from for the random rain.
+    // It currently contains the classic Matrix mix: Latin characters, Numerals, and Japanese Katakana.
+    "characters": "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレゲゼデベペオォコソトノホモヨョロゴゾドボポヴッン"
+};
